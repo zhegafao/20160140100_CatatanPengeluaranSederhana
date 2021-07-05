4f1ca70f2d661be4310497a28d151d2f933c7513
Zhega Fao Anggasta <Zhegafaoanggasta@gmail.com>
2021-07-05 19:25:26 +0700

    commit all

---
+++ app/src/main/java/com/example/tugasakhirpamzhega/utils/FunctionHelper.java
@@ -0,0 +1,23 @@
+package com.example.tugasakhirpamzhega.utils;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * Created by Fariz Ramadhan.
+ * website : https://farizdotid.com/
+ * github : https://github.com/farizdotid
+ * linkedin : https://www.linkedin.com/in/farizramadhan/
+ */
+public class FunctionHelper {
+
+    /*
+    Fungsi untuk mengconvert sebuah nilai menjadi format Rupiah.
+    Contoh : 1000 akan menjadi Rp1.000
+     */
+    public static String convertRupiah(int nominal){
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+        return formatRupiah.format(nominal);
+    }
+}
---
+++ app/src/main/java/com/example/tugasakhirpamzhega/utils/database/DaoHandler.java
@@ -0,0 +1,23 @@
+package com.example.tugasakhirpamzhega.utils.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Fariz Ramadhan.
+ * website : https://farizdotid.com/
+ * github : https://github.com/farizdotid
+ * linkedin : https://www.linkedin.com/in/farizramadhan/
+ */
+public class DaoHandler {
+    /*
+    Fungsi untuk konfigurasi Greendao. Dibuat singleton class agar tinggal panggil saja.
+     */
+    public static DaoSession getInstance(Context context) {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "catatpengeluaran_db", null);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        DaoMaster daoMaster = new DaoMaster(db);
+        return daoMaster.newSession();
+    }
+}
